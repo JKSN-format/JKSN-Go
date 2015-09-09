27f3bc0175907bbd926d0ce1d68bf274d7f854d6
Star Brilliant <m13253@hotmail.com>
2015-09-09 18:07:48 +0800

    Update

--- convert.go
+++ convert.go
@@ -2,20 +2,42 @@ package main
 
 import (
     "encoding/json"
+    "flag"
     "os"
     "./jksn"
 )
 
 func main() {
-    json_decoder := json.NewDecoder(os.Stdin)
-    var value interface{}
-    err := json_decoder.Decode(&value)
-    if err != nil {
-        panic(err.Error())
+    is_encoding := true
+    for _, arg := range flag.Args() {
+        if arg == "-d" {
+            is_encoding = false
+        }
     }
-    jksn_encoder := jksn.NewEncoder(os.Stdout)
-    err = jksn_encoder.Encode(value)
-    if err != nil {
-        panic(err.Error())
+
+    if is_encoding {
+        json_decoder := json.NewDecoder(os.Stdin)
+        var value interface{}
+        err := json_decoder.Decode(&value)
+        if err != nil {
+            panic(err.Error())
+        }
+        jksn_encoder := jksn.NewEncoder(os.Stdout)
+        err = jksn_encoder.Encode(value)
+        if err != nil {
+            panic(err.Error())
+        }
+    } else {
+        jksn_decoder := jksn.NewDecoder(os.Stdin)
+        var value interface{}
+        err := jksn_decoder.Decode(&value)
+        if err != nil {
+            panic(err.Error())
+        }
+        json_encoder := json.NewEncoder(os.Stdout)
+        err = json_encoder.Encode(value)
+        if err != nil {
+            panic(err.Error())
+        }
     }
 }
